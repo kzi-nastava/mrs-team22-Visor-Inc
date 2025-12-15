eaf625719ec2d9d1887b33e9789e04e34015caf7
Maksim Vasic <vasicmaksim4@gmail.com>
2025-12-15 10:34:08 +0100

    fead: add services;

---
+++ voom-service/src/main/java/inc/visor/voom_service/auth/service/PermissionService.java
@@ -0,0 +1,7 @@
+package inc.visor.voom_service.auth.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PermissionService {
+}
---
+++ voom-service/src/main/java/inc/visor/voom_service/domain/service/PersonService.java
@@ -0,0 +1,7 @@
+package inc.visor.voom_service.domain.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+}
