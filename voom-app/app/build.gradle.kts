d2837fed38987adcfd4b3e33a06db2e2c546b870
Maksim Vasic <vasicmaksim4@gmail.com>
2025-12-16 15:47:00 +0100

    feat: added bottom navigation

--- voom-app/app/build.gradle.kts
+++ voom-app/app/build.gradle.kts
@@ -36,6 +36,9 @@ android {
 dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
