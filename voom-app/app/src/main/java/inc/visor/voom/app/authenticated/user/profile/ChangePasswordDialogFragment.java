7814653d2f0a066db95c864ca6d3e0a7de4d07f3
nikola0234 <nikolabjelica4@gmail.com>
2025-12-18 10:44:23 +0100

    feat: Added Change password dialog.

--- voom-app/app/src/main/java/inc/visor/voom/app/authenticated/user/profile/ChangePasswordDialogFragment.java
+++ voom-app/app/src/main/java/inc/visor/voom/app/authenticated/user/profile/ChangePasswordDialogFragment.java
@@ -1,4 +1,23 @@
 package inc.visor.voom.app.authenticated.user.profile;
 
-public class ChangePasswordDialogFragment {
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import inc.visor.voom.app.R;
+
+public class ChangePasswordDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Dialog dialog = new Dialog(requireContext());
+        dialog.setContentView(R.layout.dialog_change_password);
+        dialog.setCancelable(true);
+        return dialog;
+    }
 }
--- voom-app/app/src/main/java/inc/visor/voom/app/authenticated/user/profile/ProfileFragment.java
+++ voom-app/app/src/main/java/inc/visor/voom/app/authenticated/user/profile/ProfileFragment.java
@@ -1,66 +1,80 @@
-package inc.visor.voom.app.authenticated.user;
+package inc.visor.voom.app.authenticated.user.profile;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import inc.visor.voom.app.R;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import inc.visor.voom.app.R;
+import inc.visor.voom.app.databinding.FragmentProfileBinding;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentProfileBinding binding;
+    private ProfileViewModel viewModel;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState
+    ) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(
+            @NonNull View view,
+            @Nullable Bundle savedInstanceState
+    ) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
+
+        binding.btnChangePassword.setOnClickListener(v -> {
+            new ChangePasswordDialogFragment().show(getParentFragmentManager(), "ChangePasswordDialog");
+        });
+
+        observeViewModel();
+        setupListeners();
+    }
+
+    private void observeViewModel() {
+        viewModel.getFirstName().observe(getViewLifecycleOwner(),
+                value -> binding.etFirstName.setText(value));
+
+        viewModel.getLastName().observe(getViewLifecycleOwner(),
+                value -> binding.etLastName.setText(value));
+
+        viewModel.getEmail().observe(getViewLifecycleOwner(),
+                value -> binding.etEmail.setText(value));
+    }
+
+    private void setupListeners() {
+        binding.btnSave.setOnClickListener(v ->
+                viewModel.onSaveClicked(
+                        binding.etFirstName.getText().toString(),
+                        binding.etLastName.getText().toString(),
+                        binding.etEmail.getText().toString()
+                )
+        );
+
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
+
 }
