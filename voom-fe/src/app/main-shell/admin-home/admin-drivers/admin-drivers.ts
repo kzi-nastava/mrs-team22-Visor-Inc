35ca56a362a36dcb212c1c5c3642930516d0fe2c
vasicm4 <vasicmaksim4@gmail.com>
2026-01-16 09:14:58 +0100

    feat: update admin pages forms

--- voom-fe/src/app/main-shell/admin-home/admin-drivers/admin-drivers.ts
+++ voom-fe/src/app/main-shell/admin-home/admin-drivers/admin-drivers.ts
@@ -4,6 +4,8 @@ import {MatDrawer, MatDrawerContainer, MatDrawerContent} from '@angular/material
 import {ValueInputString} from '../../../shared/value-input/value-input-string/value-input-string';
 import {MatDivider} from '@angular/material/list';
 import {MatIcon} from '@angular/material/icon';
+import {ValueInputDate} from '../../../shared/value-input/value-input-date/value-input-date';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 export const ROUTE_ADMIN_DRIVERS = "drivers";
 
@@ -18,11 +20,28 @@ export const ROUTE_ADMIN_DRIVERS = "drivers";
     MatCardContent,
     MatCardTitle,
     MatDivider,
-    MatIcon
+    MatIcon,
+    ValueInputDate,
+    ReactiveFormsModule
   ],
   templateUrl: './admin-drivers.html',
   styleUrl: './admin-drivers.css',
 })
 export class AdminDrivers {
+  userGeneralForm = new FormGroup({
+    firstName: new FormControl<string>('', [Validators.required, Validators.minLength(2), Validators.maxLength(255)]),
+    lastName: new FormControl<string>('', [Validators.required, Validators.minLength(2), Validators.maxLength(255)]),
+    birthDate: new FormControl<Date | null>(null, [Validators.required]),
+    email: new FormControl<string>('', [Validators.required, Validators.email, Validators.maxLength(255)]),
+    address: new FormControl<string>('', [Validators.required, Validators.minLength(2), Validators.maxLength(255)]),
+    phoneNumber: new FormControl<string>('', [Validators.required, Validators.minLength(2), Validators.maxLength(55)]),
+  });
 
+  protected openProfilePictureDialog() {
+
+  }
+
+  protected saveGeneralInfo() {
+
+  }
 }
--- voom-fe/src/app/main-shell/admin-home/admin-users/admin-users.ts
+++ voom-fe/src/app/main-shell/admin-home/admin-users/admin-users.ts
@@ -4,6 +4,8 @@ import {ValueInputString} from '../../../shared/value-input/value-input-string/v
 import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
 import {MatIcon} from '@angular/material/icon';
 import {MatDivider} from '@angular/material/list';
+import {ValueInputDate} from '../../../shared/value-input/value-input-date/value-input-date';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 export const ROUTE_ADMIN_USERS = "users";
 
@@ -18,11 +20,30 @@ export const ROUTE_ADMIN_USERS = "users";
     MatIcon,
     MatCardContent,
     MatCardTitle,
-    MatDivider
+    MatDivider,
+    ValueInputDate,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './admin-users.html',
   styleUrl: './admin-users.css',
 })
 export class AdminUsers {
 
+  userGeneralForm = new FormGroup({
+    firstName: new FormControl<string>('', [Validators.required, Validators.minLength(2), Validators.maxLength(255)]),
+    lastName: new FormControl<string>('', [Validators.required, Validators.minLength(2), Validators.maxLength(255)]),
+    birthDate: new FormControl<Date | null>(null, [Validators.required]),
+    email: new FormControl<string>('', [Validators.required, Validators.email, Validators.maxLength(255)]),
+    address: new FormControl<string>('', [Validators.required, Validators.minLength(2), Validators.maxLength(255)]),
+    phoneNumber: new FormControl<string>('', [Validators.required, Validators.minLength(2), Validators.maxLength(55)]),
+  });
+
+  protected openProfilePictureDialog() {
+
+  }
+
+  protected saveGeneralInfo() {
+
+  }
 }
