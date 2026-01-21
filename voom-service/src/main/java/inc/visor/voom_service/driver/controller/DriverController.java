e31a72d1569651dba519d9d67f51ab4d853d69eb
Maksim Vasic <vasicmaksim4@gmail.com>
2026-01-21 11:25:06 +0100

    feat: idk

--- voom-service/src/main/java/inc/visor/voom_service/driver/controller/DriverController.java
+++ voom-service/src/main/java/inc/visor/voom_service/driver/controller/DriverController.java
@@ -18,6 +18,8 @@ import inc.visor.voom_service.person.service.PersonService;
 import inc.visor.voom_service.ride.dto.RideResponseDto;
 import inc.visor.voom_service.vehicle.dto.VehicleSummaryDto;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -35,6 +37,7 @@ public class DriverController {
     private final UserProfileService userProfileService;
     private final UserService userService;
     private final PersonService personService;
+    private static final Logger logger = LoggerFactory.getLogger(DriverController.class);
 
     public DriverController(DriverService driverService, ActivationTokenService activationTokenService, UserProfileService userProfileService) {
     public DriverController(DriverService driverService, ActivationTokenService activationTokenService, UserService userService, PersonService personService) {
@@ -78,8 +81,11 @@ public class DriverController {
     @PutMapping("/{driverId}")
     public ResponseEntity<DriverSummaryDto> updateDriver(@PathVariable Long driverId, DriverSummaryDto driverSummaryDto) {
         Driver driver = this.driverService.getDriver(driverId).orElseThrow(NotFoundException::new);
+        logger.info("Dto: {}", driverSummaryDto);
         Person person = new Person(driver.getUser().getPerson().getId(), driverSummaryDto);
         User user = new User(driver.getUser().getId(), driverSummaryDto);
+        logger.info("User: {}", user);
+        logger.info("Person: {}", person);
         this.personService.update(person);
         this.userService.update(user);
         return ResponseEntity.ok(driverSummaryDto);
