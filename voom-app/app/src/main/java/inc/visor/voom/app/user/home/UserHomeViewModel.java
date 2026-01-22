5e65dd25b4923a7c11f58ec4eef593be919110ac
nikola0234 <nikolabjelica4@gmail.com>
2026-01-22 09:56:38 +0100

    feat: Route points on map.

--- voom-app/app/src/main/java/inc/visor/voom/app/user/home/UserHomeViewModel.java
+++ voom-app/app/src/main/java/inc/visor/voom/app/user/home/UserHomeViewModel.java
@@ -7,27 +7,9 @@ import androidx.lifecycle.ViewModel;
 import java.util.ArrayList;
 import java.util.List;
 
+import inc.visor.voom.app.user.home.model.RoutePoint;
+
 public class UserHomeViewModel extends ViewModel {
-
-    public enum PointType {
-        PICKUP, STOP, DROPOFF
-    }
-
-    public static class RoutePoint {
-        public double lat;
-        public double lng;
-        public String address;
-        public PointType type;
-        public int order;
-
-        public RoutePoint(double lat, double lng, String address, PointType type, int order) {
-            this.lat = lat;
-            this.lng = lng;
-            this.address = address;
-            this.type = type;
-            this.order = order;
-        }
-    }
 
     private final MutableLiveData<List<RoutePoint>> routePoints =
             new MutableLiveData<>(new ArrayList<>());
@@ -60,4 +42,80 @@ public class UserHomeViewModel extends ViewModel {
     public void unlockRide() {
         rideLocked.setValue(false);
     }
+
+    public void handleMapClick(double lat, double lng) {
+
+        List<RoutePoint> current = new ArrayList<>(routePoints.getValue());
+
+        if (current.isEmpty()) {
+
+            current.add(new RoutePoint(
+                    lat,
+                    lng,
+                    null,
+                    0,
+                    RoutePoint.PointType.PICKUP
+            ));
+
+        } else {
+            if (current.size() >= 1) {
+                RoutePoint last = current.get(current.size() - 1);
+                last.type = RoutePoint.PointType.STOP;
+            }
+
+            current.add(new RoutePoint(
+                    lat,
+                    lng,
+                    null,
+                    current.size(), // orderIndex = novi index
+                    RoutePoint.PointType.DROPOFF
+            ));
+        }
+        reindex(current);
+        routePoints.setValue(current);
+    }
+
+    private void reindex(List<RoutePoint> list) {
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).orderIndex = i;
+
+            if (i == 0) {
+                list.get(i).type = RoutePoint.PointType.PICKUP;
+            } else if (i == list.size() - 1) {
+                list.get(i).type = RoutePoint.PointType.DROPOFF;
+            } else {
+                list.get(i).type = RoutePoint.PointType.STOP;
+            }
+        }
+    }
+
+    public void removePoint(int index) {
+
+        List<RoutePoint> current = new ArrayList<>(routePoints.getValue());
+
+        if (index < 0 || index >= current.size()) return;
+
+        current.remove(index);
+
+        reindex(current);
+
+        routePoints.setValue(current);
+    }
+
+    public void setAsDropoff(int index) {
+
+        List<RoutePoint> current = new ArrayList<>(routePoints.getValue());
+
+        if (index <= 0 || index >= current.size()) return;
+
+        RoutePoint selected = current.remove(index);
+        current.add(selected);
+
+        reindex(current);
+
+        routePoints.setValue(current);
+    }
+
+
+
 }
