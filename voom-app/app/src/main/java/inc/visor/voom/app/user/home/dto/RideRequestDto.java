0e8d5eeb4dcdeefe9941e0c26873449502197b4e
nikola0234 <nikolabjelica4@gmail.com>
2026-01-22 11:57:47 +0100

    feat: Creating ride request payload.

--- voom-app/app/src/main/java/inc/visor/voom/app/user/home/dto/RideRequestDto.java
+++ voom-app/app/src/main/java/inc/visor/voom/app/user/home/dto/RideRequestDto.java
@@ -1,4 +1,34 @@
 package inc.visor.voom.app.user.home.dto;
 
+import java.util.List;
+
 public class RideRequestDto {
+
+    public Route route;
+    public Schedule schedule;
+    public Integer vehicleTypeId;
+    public Preferences preferences;
+    public List<String> linkedPassengers;
+
+    public static class Route {
+        public List<Point> points;
+    }
+
+    public static class Point {
+        public double lat;
+        public double lng;
+        public int orderIndex;
+        public String type;
+        public String address;
+    }
+
+    public static class Schedule {
+        public String type;
+        public String startAt;
+    }
+
+    public static class Preferences {
+        public boolean pets;
+        public boolean baby;
+    }
 }
