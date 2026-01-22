a72cd1a6d2c558cdd891e3c3b204647d9b0a6aa6
nikola0234 <nikolabjelica4@gmail.com>
2026-01-22 13:41:05 +0100

    feat: Implemented driver simulation on map.

---
+++ voom-app/app/src/main/java/inc/visor/voom/app/shared/model/FreeDriverSnapshot.java
@@ -0,0 +1,4 @@
+package inc.visor.voom.app.shared.model;
+
+public class FreeDriverSnapshot {
+}
---
+++ voom-app/app/src/main/java/inc/visor/voom/app/shared/simulation/DriverSimulationManager.java
@@ -0,0 +1,4 @@
+package inc.visor.voom.app.shared.simulation;
+
+public class DriverSimulationManager {
+}
