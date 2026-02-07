57d9cafea305fad1a4a2c058a0513c6e68f04596
nikola0234 <nikolabjelica4@gmail.com>
2026-02-07 14:38:39 +0100

    feat: Admin users fragment.

---
+++ voom-app/app/src/main/java/inc/visor/voom/app/admin/users/adapter/UserViewHolder.java
@@ -0,0 +1,24 @@
+package inc.visor.voom.app.admin.users.adapter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jspecify.annotations.NonNull;
+
+import inc.visor.voom.app.R;
+
+public class UserViewHolder extends RecyclerView.ViewHolder {
+
+    TextView tvName, tvEmail, tvRole, tvStatus;
+
+    public UserViewHolder(@NonNull View itemView) {
+        super(itemView);
+        tvName = itemView.findViewById(R.id.tvName);
+        tvEmail = itemView.findViewById(R.id.tvEmail);
+        tvRole = itemView.findViewById(R.id.tvRole);
+        tvStatus = itemView.findViewById(R.id.tvStatus);
+    }
+}
+
